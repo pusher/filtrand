b3913c0a6f0ccce71c011434253df4151c8b6272
Mary Rose Cook <maryrosecook@maryrosecook.com>
2011-11-08 17:24:57 +0000

    remove old stuff from realtime pusher

--- wikipedia-source.js
+++
@@ -1,8 +0,0 @@
-var irc = require('irc');
-var ircClient = new irc.Client('irc.wikimedia.org', 'pusher-streamer', {
-  channels: ['#en.wikipedia']
-});
-
-ircClient.addListener('message', function (from, to, message) {
-  console.log(from + ' => ' + to + ': ' + message);
-});
